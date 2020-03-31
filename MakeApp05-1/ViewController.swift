898594e942726734139b5e73736f4869ce0bb546
yang <heroparkjisung2gmail.com>
2020-03-31 14:48:43 +0900

    Initial Commit

---
+++ MakeApp05-1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MakeApp05-1
+//
+//  Created by Y on 2020/03/31.
+//  Copyright © 2020 HEETAE YANG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
